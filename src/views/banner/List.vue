783d20b18775bbd770e92370840fdc8d5e374f64
tresor <814908133@qq.com>
2018-11-09 11:30:49 +0800

    UI

--- src/views/banner/List.vue
+++ src/views/banner/List.vue
@@ -1,3 +1,17 @@
 <template>
-    <div></div>
+    <div>
+        <!-- 顶部工具条 -->
+        <div class="tools-bar">
+            <div class="button-list">
+                <el-select placeholder="请选择显示位置">
+                    <el-option label="首页" value="1"></el-option>
+                    <el-option label="师生作品" value="2"></el-option>
+                </el-select>
+                <el-input v-model="searchVal" placeholder="请输入关键字"></el-input>
+                <el-button size="small" class="search-btn" @click.native="onSeach">搜索</el-button>
+                <el-button size="small" type="danger" class="add-btn" @click="onAdd">新增</el-button>
+            </div>
+        </div>
+        
+    </div>
 </template>
